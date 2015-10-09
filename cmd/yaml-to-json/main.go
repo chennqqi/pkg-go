4bf2c0a1f185ed5923c1fee57f354f32e9c205bf
Peter Edge <peter.edge@gmail.com>
2015-10-09 01:30:15 -0700

    add yaml

---
+++ cmd/yaml-to-json/main.go
@@ -0,0 +1,41 @@
+/*
+Package main implements the yaml2json command-line tool, which takes
+a YAML file as stdin, and outputs a JSON file to stdout.
+
+See the README at https://github.com/peter-edge/go-yaml2json/blob/master/README.md for more details.
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"go.pedge.io/pkg/yaml"
+)
+
+func main() {
+	if err := do(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func do() error {
+	var pretty bool
+	flag.BoolVar(&pretty, "pretty", false, "Make the JSON output pretty.")
+	flag.Parse()
+
+	yamlData, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+	jsonData, err := pkgyaml.ToJSON(yamlData, pkgyaml.ToJSONOptions{Pretty: pretty})
+	if err != nil {
+		return err
+	}
+	_, err = os.Stdout.Write(jsonData)
+	return err
+}
