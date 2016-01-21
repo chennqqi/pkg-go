d96725fbfad1569e251cf38cf3d95dbfa87b1288
Peter Edge <peter.edge@gmail.com>
2016-01-21 11:57:34 -0500

    test error

--- memoize/pkgmemoize_test.go
+++ memoize/pkgmemoize_test.go
@@ -1,6 +1,7 @@
 package pkgmemoize
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -32,6 +33,36 @@ func TestFibonacciEMemoFunc(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, uint64(12200160415121876738), result.(uint64))
 	require.Equal(t, 94, calls)
+}
+
+func TestFibonacciEMemoFuncError(t *testing.T) {
+	calls := 0
+	fibonacci := NewEMemoFunc(
+		func(i int, eMemoFunc EMemoFunc) (interface{}, error) {
+			calls++
+			if i == 0 {
+				return uint64(0), nil
+			}
+			if i == 1 {
+				return uint64(1), nil
+			}
+			if i == 5 {
+				return 0, fmt.Errorf("5")
+			}
+			n1, err := eMemoFunc.Do(i - 1)
+			if err != nil {
+				return 0, err
+			}
+			n2, err := eMemoFunc.Do(i - 2)
+			if err != nil {
+				return 0, err
+			}
+			return n1.(uint64) + n2.(uint64), nil
+		},
+	)
+	_, err := fibonacci.Do(93)
+	require.Error(t, err, "5")
+	require.Equal(t, 89, calls)
 }
 
 func TestFibonacciMemoFunc(t *testing.T) {
