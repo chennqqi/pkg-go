8a11a67a8b727f4961bd30f0b0957c226fed9420
Peter Edge <peter.edge@gmail.com>
2016-01-27 01:12:27 +0100

    errors
    Signed-off-by: Peter Edge <peter.edge@gmail.com>

--- errors/pkgerrors.go
+++ errors/pkgerrors.go
@@ -6,62 +6,30 @@ import (
 	"fmt"
 )
 
-// With adds the key/value pairs and returns a new Builder.
-func With(keyValues ...interface{}) *Builder {
-	return &Builder{getFields(keyValues, nil)}
-}
-
 // New creates a new error.
-func New(args ...interface{}) error {
-	return errors.New(fmt.Sprint(args...))
-}
-
-// Builder is an error builder.
-type Builder struct {
-	fields map[string]interface{}
-}
-
-// With adds the key/value pairs and returns a new Builder.
-func (b *Builder) With(keyValues ...interface{}) *Builder {
-	return &Builder{getFields(keyValues, b.fields)}
-}
-
-// New creates a new error.
-func (b *Builder) New(args ...interface{}) error {
-	buffer := bytes.NewBuffer(nil)
-	var message string
-	if len(args) > 0 {
-		message = fmt.Sprint(args...)
+//
+// The first argument is the message, the rest are key/value pairs.
+func New(message string, keyValues ...interface{}) error {
+	len := len(keyValues)
+	if len == 0 {
+		return errors.New(message)
 	}
+	buffer := bytes.NewBuffer(nil)
 	if message != "" {
 		_, _ = buffer.WriteString(message)
-		if len(b.fields) > 0 {
-			_ = buffer.WriteByte(' ')
-		}
+		_ = buffer.WriteByte(' ')
 	}
-	first := false
-	for key, value := range b.fields {
-		if !first {
+	if len%2 != 0 {
+		keyValues = append(keyValues, "MISSING")
+		len++
+	}
+	for i := 0; i < len; i += 2 {
+		if i != 0 {
 			_ = buffer.WriteByte(' ')
-			first = true
 		}
-		_, _ = buffer.WriteString(key)
+		_, _ = buffer.WriteString(fmt.Sprintf("%v", keyValues[i]))
 		_ = buffer.WriteByte('=')
-		_, _ = buffer.WriteString(fmt.Sprintf("%v", value))
+		_, _ = buffer.WriteString(fmt.Sprintf("%v", keyValues[i+1]))
 	}
 	return errors.New(buffer.String())
-}
-
-func getFields(keyValues []interface{}, existingFields map[string]interface{}) map[string]interface{} {
-	if len(keyValues)%2 != 0 {
-		keyValues = append(keyValues, "MISSING")
-	}
-	fields := make(map[string]interface{}, len(keyValues)/2)
-	for i := 0; i < len(keyValues); i += 2 {
-		fields[fmt.Sprintf("%v", keyValues[i])] = keyValues[i+1]
-	}
-	for key, value := range existingFields {
-		fields[key] = value
-	}
-	return fields
 }
